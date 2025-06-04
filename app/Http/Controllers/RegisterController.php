918e0c5ae361c7e786b69ab1969d32836b898dad
Lucasvs06 <dls0009@edu.curio.nl>
2025-06-05 00:30:07 +0200

    rollback.

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+{
+    $validated = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|string|min:6',
+    ]);
+
+    $user = User::create([
+        'name' => $validated['name'],
+        'email' => $validated['email'],
+        'password' => bcrypt($validated['password']),
+        'role' => 'gambler', // standaardrol
+        'balance' => 50.00   // startsaldo
+    ]);
+
+    $token = $user->createToken('api_token')->plainTextToken;
+
+    return response()->json(['token' => $token, 'user' => $user]);
+}
+
+}
